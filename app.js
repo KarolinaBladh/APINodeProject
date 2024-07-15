b1236ebfd9524008becb4ed7f7d15187c6b0175c
KarolinaBladh <skogstrollet1982@yahoo.se>
2024-07-15 14:55:44 +0200

    add middleware app.use

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 
+const feedRoutes = require("./routes/feed");
+
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "default-src 'none'");
+  next();
+});
+
+app.use("/feed", feedRoutes);
 
 app.listen(8080);
